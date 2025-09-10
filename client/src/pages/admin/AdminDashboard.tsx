60b1c81ea2e818bfd1ab15e851eeb21b8879717b
Stevan Revalino <stevanrevalino08@gmail.com>
2025-09-10 14:58:28 +0700

    feat: Refactor layout components; move AdminSidebar, Header, and Footer to layouts directory

--- client/src/pages/admin/AdminDashboard.tsx
+++ client/src/pages/admin/AdminDashboard.tsx
@@ -1,5 +1,5 @@
 import { useState } from "react";
-import AdminSidebar from "../../components/AdminSidebar";
+import AdminSidebar from "../../layouts/AdminSidebar";
 import KelolaDataMurid from "./KelolaDataMurid";
 import KelolaDataKonseling from "./KelolaDataKonseling";
 
