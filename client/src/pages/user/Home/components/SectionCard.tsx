ed9a90129407b6a1bd9aa8178550b4bbfc45900d
Stevan Revalino <stevanrevalino08@gmail.com>
2025-09-24 14:13:08 +0700

    fix: update SectionCard component styles for consistent rounded corners

--- client/src/pages/user/Home/components/SectionCard.tsx
+++ client/src/pages/user/Home/components/SectionCard.tsx
@@ -12,7 +12,7 @@ const SectionCard = ({
   className = "",
 }: SectionCardProps) => {
   const containerClass =
-    "shadow-[0_6px_12px_rgba(0,0,0,0.15)] rounded-br-[40px] lg:rounded-br-[60px] rounded-tl-[40px] lg:rounded-tl-[60px] bg-[#e6f3ff] px-4 lg:px-10 py-4 lg:py-6";
+    "shadow-[0_6px_12px_rgba(0,0,0,0.15)] rounded-br-2xl lg:rounded-br-4xl rounded-tl-2xl lg:rounded-tl-4xl bg-[#e6f3ff] px-4 lg:px-10 py-4 lg:py-6";
 
   const headerPositionClass =
     "absolute -top-4 lg:-top-6 left-8 lg:left-16 bg-[#31A4FF] rounded-4xl px-3 lg:px-4 py-1 font-bold text-sm lg:text-lg";
