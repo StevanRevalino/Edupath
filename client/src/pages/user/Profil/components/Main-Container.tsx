7e3459970e22866874d351e415576beaab1b8118
Stevan Revalino <stevanrevalino08@gmail.com>
2025-10-10 16:40:00 +0700

    refactor: adjust padding in MainContainer and remove profile avatar positioning

--- client/src/pages/user/Profil/components/Main-Container.tsx
+++ client/src/pages/user/Profil/components/Main-Container.tsx
@@ -32,22 +32,14 @@ export default function MainContainer({
   }, [onHeightChange]);
 
   return (
-    <div className="flex-1 pt-[180px] pb-10">
+    <div className="flex-1 pt-[240px] pb-10">
       {/* Container dengan position relative untuk avatar yang menonjol */}
       <div className="relative max-w-4xl mx-auto">
-        {/* Profile Avatar - Positioned absolutely to protrude from container */}
-        {isProfilePage && (
-          <div className="absolute -top-15 left-1/2 transform -translate-x-1/2 z-20">
-            <div className="w-30 h-30 bg-gray-200 rounded-full border-4 border-[#00437A] flex items-center justify-center shadow-xl">
-              <User size={75} className="text-gray-500" />
-            </div>
-          </div>
-        )}
 
         {/* Main Content Container */}
         <div
           ref={containerRef}
-          className="bg-white rounded-t-[75px] shadow-2xl p-10 pt-15 border-[#00437A] border-3 border-b-0 min-h-[600px] relative z-[2]"
+          className="rounded-t-[75px] p-10 pt-15 border-b-0 min-h-[600px] relative z-[2]"
         >
           {children}
         </div>
